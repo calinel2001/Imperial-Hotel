ec9a51342737558fecc7e42f6d22a8fd93b14663
Chelcea Calin Marius <76866499+chelceacalin@users.noreply.github.com>
2021-04-24 22:40:02 +0300

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,21 +1,18 @@
 <?php
-$name=$_POST['name'];
-$visitor_email=$_POST['email'];
-$message=$_POST['message'];
-
-$email_from='chelcea.calin@yahoo.com';
-$email_subject ="New form Submission";
-$email_body="User Name: $name.\n".
-               "User Email: $visitor_email.\n".
-                    "User Message: $message.\n";
-
-
-                    $to="chelceacalin1@gmail.com";
-                    $headers ="From: $email_from \r\n";
-                    $headers .="Reply-to: $visitor_email \r\n";
-                    mail($to,$email_subject,$email_body,$headers);
-
-                    header("Location: index.html");
-
-
+$action=$_REQUEST['action'];
+    {
+    $name=$_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $message=$_REQUEST['message'];
+    if (($name=="")||($email=="")||($message==""))
+        {
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        }
+    else{        
+        $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+        mail("chelceacalin@yahoo.com", $subject, $message, $from);
+        echo "Email sent!";
+        }
+    }  
 ?>
